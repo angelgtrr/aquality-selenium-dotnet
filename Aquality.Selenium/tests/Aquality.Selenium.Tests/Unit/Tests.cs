6eb49be2440830f59f5ff06866060a31a7a74682
a2.lopez <56662608+angelgtrr@users.noreply.github.com>
2025-04-11 13:08:48 -0600

    test

---
+++ Aquality.Selenium/tests/Aquality.Selenium.Tests/Unit/Tests.cs
@@ -0,0 +1,22 @@
+﻿using Aquality.Selenium.Browsers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aquality.Selenium.Tests.Unit
+{
+    [TestFixture]
+    [Parallelizable(ParallelScope.Children)]
+    internal class Tests
+    {
+        [Test]
+        public void AddCookie()
+        {
+            AqualityServices.Browser.Driver.Manage().Cookies.AddCookie(new OpenQA.Selenium.Cookie("key", "value")); 
+        }
+
+    }
+}
